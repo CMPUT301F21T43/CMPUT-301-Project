db0a3c4d6af1dcd147c37db5782ccc4b8048acd6
Marty Rudolf <54113035+marty-rudolf@users.noreply.github.com>
2021-11-05 11:02:35 -0600

    Completed unit testing for User class.

---
+++ CMPUT301Project/app/src/test/java/com/example/GroupProject/UserUnitTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2021-2022. Group 43 CMPUT301 F2021
+ * All rights reserved.
+ */
+
+package com.example.GroupProject;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.lang.reflect.Field;
+
+public class UserUnitTest {
+    public User testUser;
+
+    @Before
+    public void setUpUser() {
+        testUser = new User("Bob Dylan", "Robert", "Dylan");
+    }
+
+    @Test
+    public void testGetUsername() {
+        assertEquals(testUser.getUsername(), "Bob Dylan");
+    }
+
+    @Test
+    public void testGetFirstName() {
+        assertEquals(testUser.getFirstName(), "Robert");
+    }
+
+    @Test
+    public void testGetLastName() {
+        assertEquals(testUser.getLastName(), "Dylan");
+    }
+
+    @Test
+    public void testSetUserName() throws NoSuchFieldException, IllegalAccessException{
+        testUser.setUsername("Bobby Dylan");
+        final Field field = testUser.getClass().getDeclaredField("username");
+        field.setAccessible(true);
+        assertEquals(field.get(testUser), "Bobby Dylan");
+    }
+
+    @Test
+    public void testSetFirstName() throws NoSuchFieldException, IllegalAccessException{
+        testUser.setFirstName("Bobby");
+        final Field field = testUser.getClass().getDeclaredField("firstName");
+        field.setAccessible(true);
+        assertEquals(field.get(testUser), "Bobby");
+    }
+    @Test
+    public void testSetLastName() throws NoSuchFieldException, IllegalAccessException{
+        testUser.setLastName("Dyl");
+        final Field field = testUser.getClass().getDeclaredField("lastName");
+        field.setAccessible(true);
+        assertEquals(field.get(testUser), "Dyl");
+    }
+}
